2fe3860e2f8944d3239bac0d896a456270906a91
Anvilfolk <anvilfolk@gmail.com>
2013-05-13 03:53:24 -0700

    - tralala

--- ng.pl
+++ ng.pl
@@ -44,17 +44,23 @@
 
 :- chrg_symbols word/3, g/1, update/1, wait/1, remove_update/1, iCat/3, ambiguity/0.
 
-:- chr_constraint clean/0, checkExistentialConstraints/0, unsat/1, expansion_phase/0, constraint_phase/0, tpl/1.
+:- chr_constraint clean/0, checkExistentialConstraints/0, unsat/1,
+   expansion_phase/0, constraint_phase/0, constraint_go/0, tpl/1,
+   doneLeft/1, doneRight/1, done/1.
 
 % LEXICON: word(Category,Traits,Word) 
 
   [le] ::> word(det,[sing,masc],le).
   [les] ::> word(det,[plur,masc],le).
+  [un] ::> word(det,[sing,masc],un).
+  [une] ::> word(det,[sing,fem],un).
   [tres] ::> word(adv,[sing,masc],tres).
   
   %nouns
   [livre] ::> word(n,[sing,masc],livre).
   [livres] ::> word(n,[plur,masc],livre).
+
+  [pomme] ::> word(n,[sing,fem],pomme).
 
   %adjectives
   [jaune] ::> word(adj,[sing,masc],jaune).
@@ -65,9 +71,11 @@
 
   %aux verb
   [a] ::> word(v,[sing, na], a).
+  [mange] ::> word(v,[sing, na], mange).
 
   %proper name
   [amelie] ::> word(pn,[sing, feme], amelie).
+  [jean] ::> word(pn,[sing, masc], jean).
 
 
 % CREATE INSTANTIATED CATEGORIES FROM WORDS
@@ -96,31 +104,34 @@ word(Comp, Attr, Word) <:> iCat(Comp, Attr, Word).
 
  % RUN SINGLE EXAMPLE
 
-doParse(S) :- unsat([]), init_grammar, expansion_phase, parse(S), checkExistentialConstraints.
+doParse(S) :- unsat([]), init_grammar, parse(S), expansion_phase, constraint_go, checkExistentialConstraints.
 
 % GRAMMAR
 %
 %
 % INIT GRAMMAR
-   init_grammar :- tpl(obligatority(np, n)), tpl(obligatority(np, det)),
-		   tpl(constituency(np, det)), tpl(constituency(np, adj)), tpl(constituency(np, n)), tpl(constituency(np, adv)), tpl(constituency(np, pn)),
+   init_grammar :- % tpl(obligatority(np, n)), % NPs can also have personal nouns
+		   tpl(constituency(np, [det, adj, n, adv, pn])),
 		   tpl(precedence(np, det,adj)), tpl(precedence(np, det,n)),
   		 tpl(precedence(np, adj,n)), tpl(precedence(np, sup,adj)), tpl(precedence(np, n,sup)), tpl(precedence(np, det,sup)),
 		   tpl(unicity(np, det)),
-		   tpl(requirement(np, n,det)),
+		   tpl(requirement(np, n, det)),
+		   tpl(exclusion(np, pn, det)),
 		   tpl(dependence(np, det,n)),
-		   tpl(dependence(np, n,adj)).
+		   tpl(dependence(np, n,adj)),
+		   tpl(constituency(vp, [v,np])).
 
+init_grammar :- tpl()
 
 % CATEGORIES, KERNELS, ETC
 
+category(sentence)
 category(np).
 category(vp).
-%category(adjp).
 
 kernel(n, np).
+kernel(pn, np).
 kernel(v, vp).
-%kernel(adj, adjp).
 
 % TREE RULES
 %
@@ -137,28 +148,53 @@ iCat(Comp,Attr,Tree) ::> kernel(Comp, Cat) | iCat(Cat,Attr,Tree).
 
 % EXPANDING CATEGORIES
 
-  !iCat(Comp,_,Tree1), iCat(Cat,Attr2,Tree2),         % Comp next to Cat, subcategories stay, to-expand category disappears
-  !{tpl(constituency(Cat, Comp))}, !{expansion_phase} % Comp can be a constituent of Cat
-  <:> buildTree(Cat, Tree1, Tree2, Tree)              % Build the tree for the expanded Cat
-  | iCat(Cat, Attr2, Tree).                           % Create expanded Cat, replacing the smaller, previous one
+  !iCat(Comp, Attr1, Tree1):(N1,N2), iCat(Cat, Attr2, Tree2),     % Comp next to Cat, subcategories stay, to-expand category disappears
+  !{done(iCat(N1,N2,Comp, Attr1, Tree1))},
+  !{tpl(constituency(Cat, L))}, !{expansion_phase}        % L is the set of constituents of Cat
+  <:> member(Comp, L), buildTree(Cat, Tree1, Tree2, Tree) % Comp is constituent of Cat, build the tree for the expanded Cat
+  | iCat(Cat, Attr2, Tree).                               % Create expanded Cat, replacing the smaller, previous one
 
-  iCat(Cat,Attr1,Tree1), !iCat(Comp,_,Tree2),         % Symmetric rule
-  !{tpl(constituency(Cat, Comp))}, !{expansion_phase}
-  <:> buildTree(Cat, Tree1, Tree2, Tree)
+  iCat(Cat, Attr1, Tree1), !iCat(Comp, Attr2, Tree2):(N1,N2),         % Symmetric rule
+  !{done(iCat(N1,N2,Comp, Attr2, Tree2))},
+  !{tpl(constituency(Cat, L))}, !{expansion_phase}
+  <:> member(Comp, L), buildTree(Cat, Tree1, Tree2, Tree)
   | iCat(Cat, Attr1, Tree).
 
-% Same rules using <=> instead of <:>
-%iCat(N1, N2, Comp,_,Tree1), tpl(constituency(Cat, Comp)), expansion_phase \ iCat(N2, N3, Cat, Attr2, Tree2) <=>
-%   buildTree(Cat, Tree1, Tree2, Tree)
-%   | iCat(N1, N3, Cat, Attr2, Tree).
 
-%iCat(N2, N3, Comp,_,Tree2), tpl(constituency(Cat, Comp)), expansion_phase \ iCat(N1, N2, Cat, Attr1, Tree1) <=>
-%   buildTree(Cat, Tree1, Tree2, Tree)
-%   | iCat(N1, N3, Cat, Attr1, Tree).
+% Checking that categories are done expanding.
+
+iCat(N1, N2, Cat, Attr, Tree) ==> not(category(Cat)) | done(iCat(N1, N2, Cat, Attr, Tree)).
+
+% Check that there is no expansion possible on the left.
+% Either this is the first word
+
+  iCat(0, N, Cat, Attr, Tree) ==> doneLeft(iCat(0, N, Cat, Attr, Tree)).
+
+% Or the left-category is not a constituent
+
+  iCat(_, N2, Comp,_,_), iCat(N2, N3, Cat,Attr,Tree),
+  tpl(constituency(np, L))
+  ==> not(member(Comp, L)) 
+  | doneLeft(iCat(N2, N3, Cat, Attr, Tree)).
+  
+% Same for the right
+
+  iCat(N1, N2, Cat, Attr, Tree), all(0, N2) ==> doneRight(iCat(N1, N2, Cat, Attr, Tree)).
+
+  iCat(N1, N2, Cat, Attr, Tree), iCat(N2, _, Comp, _, _), 
+  tpl(constituency(np, L))
+  ==> not(member(Comp, L))
+  | doneRight(iCat(N1, N2, Cat, Attr, Tree)).
+
+doneLeft(Prop), doneRight(Prop) <=> done(Prop).
 
 % Once category expansion is done, i.e. no more of the above rules can be applied,
 % change to the constraint phase where constraints can be checked.
-expansion_phase <=> constraint_phase.
+
+expansion_phase, constraint_go <=> constraint_phase.
+%constraint_phase \ doneLeft(_) <=> true.
+%constraint_phase \ doneRight(_) <=> true.
+%constraint_phase \ done(_) <=> true.
 
 % Once we are in the constraint phase, check whether there is ambiguity.
 % For this, we try to find two non-terminal categories that overlap.
@@ -190,10 +226,10 @@ iCat(N1, N2, _, _, _), iCat(N3, N4, _, _, _), constraint_phase ==> N1 < N3, N3 <
   ::> wait(obligatority(Cat, C)).                 % then wait for C...
   
   
-  !{iCat(N1, N2, C,_,_)},                               % Found C!
-  wait(obligatority(_,_,C)):(N3, N4),{constraint_phase} % We needed C within some bounds
-  <:> N3 =< N1, N2 =< N4                                % C is within those bounds
-  | true.                                               % Obligatority satisfied!
+  !{iCat(N1, N2, C,_,_)},                                 % Found C!
+  wait(obligatority(_,C)):(N3, N4), !{constraint_phase} % We needed C within some bounds
+  <:> N3 =< N1, N2 =< N4                                  % C is within those bounds
+  | true.                                                 % Obligatority satisfied!
 
 % REQUIREMENT (existential property)
 % requirement(C,C1,C2) - if there is a C1 in C, then there must also be a C2
@@ -206,10 +242,10 @@ iCat(N1, N2, _, _, _), iCat(N3, N4, _, _, _), constraint_phase ==> N1 < N3, N3 <
   ::> N3 =< N1, N2 =< N4                                 % C1 within C2
   | wait(requirement(Cat,C1,C2)):(N3, N4).               % then wait...
   
-  !{iCat(N1, N2, C2,_,_)},                               % Found C2!
-  wait(requirement(_,_,C2)):(N3, N4), {constraint_phase} % There was a requirement for C2
-  <:> N3 =< N1, N2 =< N4                                 % C2 within C
-  | true.                                                % Requirement satisfied!
+  !{iCat(N1, N2, C2,_,_)},                                % Found C2!
+  wait(requirement(_,_,C2)):(N3, N4), !{constraint_phase} % There was a requirement for C2
+  <:> N3 =< N1, N2 =< N4                                  % C2 within C
+  | true.                                                 % Requirement satisfied!
 
 %
 
